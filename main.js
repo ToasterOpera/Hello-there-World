8cccd4f380a233601304f85642102ea919e974f7
ToasterOpera <john.charles.monteith@gmail.com>
2020-09-22 20:44:49 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = radius * radius * PI;
+radius = 4;
+area = radius * radius * PI;
+
+function hellothere() {
+    document.getElementById("conv").src = "oby.png";
+}
+
+function general() {
+    document.getElementById("conv").src = "General.gif";
+}
